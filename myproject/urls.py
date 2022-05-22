08c9d7a228665fe6ad1710e1708a5113e62cde97
Raul Santos <raul.santos.calvo@gmail.com>
2022-05-22 10:46:53 +0000

    final project submission with css fixes

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
+    path('', lambda request: redirect('onlinecourse/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
